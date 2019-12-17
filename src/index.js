657d1a0d0db826e7fb38d28f2d6f1e066ee44f46
maurisan74 <44737587+maurisan74@users.noreply.github.com>
2019-12-17 00:25:49 -0300

    17-12

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ app.use('/api/empleados', require('./routes/empleados'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/instalaciones', require('./routes/instalaciones'));
 app.use('/api/tiposTrabajos', require('./routes/tipostrabajos'));
+app.use('/api/equipos', require('./routes/equipos'));
+
 //app.use('/api/clientes', require('./routes/clientes'));
 
 //middlewares
---
+++ src/routes/equipos.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+const router = Router();
+
+
+const equipos = require('../FilesJson/equipos.json');
+router.get('/', (req, res) => {
+    res.json(equipos);
+});
+
+
+module.exports = router;
