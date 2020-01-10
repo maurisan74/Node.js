2e212b7a2766cf2387ae5d4ec3c255323d432459
maurisan74 <44737587+maurisan74@users.noreply.github.com>
2020-01-09 22:37:17 -0300

    Master
    Master

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 const express=require('express');
 const app=express();
 const morgan = require('morgan');
+var favicon = require('serve-favicon')
 
 //settings
 app.set('port', 3000);
 
 // routes
+//var app = express()
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(require('./routes/index'));
-
+//app.listen(3000)
 app.use('/api/clientes',require('./routes/clientes'));
 app.use('/api/empleados', require('./routes/empleados'));
 app.use('/api/proyectos', require('./routes/proyectos'));
@@ -25,5 +28,5 @@ app.use(express.urlencoded({extended:true}));
  
 //startind the server
 app.listen(app.get('port'), ()=>{
-    console.log(`Server en port ${app.get('port')}`)
-});
+        console.log(`Server en port ${app.get('port')}`)
+    });
--- src/routes/equipos.js
+++ src/routes/equipos.js
@@ -6,6 +6,4 @@ const equipos = require('../FilesJson/equipos.json');
 router.get('/', (req, res) => {
     res.json(equipos);
 });
-
-
 module.exports = router;
